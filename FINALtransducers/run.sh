cfe26765d08c8eaafca448795a261021ff7bc159
João Tiago Aparício <joaotiagoaparicio@gmail.com>
2019-11-12 17:16:13 +0000

    run main updated for solved automata

---
+++ FINALtransducers/run.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+
+################### letras ################
+#
+# Compila e gera a versão gráfica do lemma2noun
+fstcompile --isymbols=syms.txt --osymbols=syms.txt  lemma2noun.txt | fstarcsort > lemma2noun.fst
+fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait lemma2noun.fst | dot -Tpdf  > lemma2noun.pdf
+# Compila e gera a versão gráfica do teste aluno
+fstcompile --isymbols=syms.txt --osymbols=syms.txt  aluno.txt | fstarcsort > aluno.fst
+fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait aluno.fst | dot -Tpdf  > aluno.pdf
+
+
+
+# Compila e gera a versão gráfica do lemma2adverb
+fstcompile --isymbols=syms.txt --osymbols=syms.txt  lemma2adverb.txt | fstarcsort > lemma2adverb.fst
+fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait lemma2adverb.fst | dot -Tpdf  > lemma2adverb.pdf
+# Compila e gera a versão gráfica do teste inteligentemente
+fstcompile --isymbols=syms.txt --osymbols=syms.txt  inteligentemente.txt | fstarcsort > inteligentemente.fst
+fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait inteligentemente.fst | dot -Tpdf  > inteligentemente.pdf
+
+
+
+# Compila e gera a versão gráfica do lemma2verbip
+fstcompile --isymbols=syms.txt --osymbols=syms.txt  lemma2verbip.txt | fstarcsort > lemma2verbip.fst
+fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait lemma2verbip.fst | dot -Tpdf  > lemma2verbip.pdf
+# Compila e gera a versão gráfica do teste lavar
+fstcompile --isymbols=syms.txt --osymbols=syms.txt  lavar.txt | fstarcsort > lavar.fst
+fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait lavar.fst | dot -Tpdf  > lavar.pdf
+
+
+################### Testa os tradutores ################
+
+echo "Transdutor aluno :"
+fstrmepsilon aluno.fst | fsttopsort | fstprint --isymbols=syms.txt
+echo "Output: "
+fstcompose aluno.fst lemma2noun.fst  > resultado.fst
+fstproject --project_output resultado.fst | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=syms.txt | awk '{print $3}'
+
+
+echo "Transdutor inteligentemente :"
+fstrmepsilon inteligentemente.fst | fsttopsort | fstprint --isymbols=syms.txt
+echo "Output: "
+fstcompose inteligentemente.fst lemma2adverb.fst  > resultado.fst
+fstproject --project_output resultado.fst | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=syms.txt | awk '{print $3}'
+
+
+echo "Transdutor lavar :"
+fstrmepsilon lavar.fst | fsttopsort | fstprint --isymbols=syms.txt
+echo "Output: "
+fstcompose lavar.fst lemma2verbip.fst  > resultado.fst
+fstproject --project_output resultado.fst | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=syms.txt | awk '{print $3}'
+
