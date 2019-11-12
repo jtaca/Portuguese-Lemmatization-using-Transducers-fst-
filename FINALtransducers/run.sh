7073fc56ecece8051f345f9b9e3addbf7b8810d8
João Tiago Aparício <joaotiagoaparicio@gmail.com>
2019-11-12 17:53:25 +0000

    verbif solved

--- FINALtransducers/run.sh
+++ FINALtransducers/run.sh
@@ -11,14 +11,12 @@ fstcompile --isymbols=syms.txt --osymbols=syms.txt  aluno.txt | fstarcsort > alu
 fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait aluno.fst | dot -Tpdf  > aluno.pdf
 
 
-
 # Compila e gera a versão gráfica do lemma2adverb
 fstcompile --isymbols=syms.txt --osymbols=syms.txt  lemma2adverb.txt | fstarcsort > lemma2adverb.fst
 fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait lemma2adverb.fst | dot -Tpdf  > lemma2adverb.pdf
 # Compila e gera a versão gráfica do teste inteligentemente
 fstcompile --isymbols=syms.txt --osymbols=syms.txt  inteligentemente.txt | fstarcsort > inteligentemente.fst
 fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait inteligentemente.fst | dot -Tpdf  > inteligentemente.pdf
-
 
 
 # Compila e gera a versão gráfica do lemma2verbip
@@ -29,10 +27,14 @@ fstcompile --isymbols=syms.txt --osymbols=syms.txt  lavar.txt | fstarcsort > lav
 fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait lavar.fst | dot -Tpdf  > lavar.pdf
 
 
-
 # Compila e gera a versão gráfica do lemma2verbis
 fstcompile --isymbols=syms.txt --osymbols=syms.txt  lemma2verbis.txt | fstarcsort > lemma2verbis.fst
 fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait lemma2verbis.fst | dot -Tpdf  > lemma2verbis.pdf
+
+
+# Compila e gera a versão gráfica do lemma2verbif
+fstcompile --isymbols=syms.txt --osymbols=syms.txt  lemma2verbif.txt | fstarcsort > lemma2verbif.fst
+fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait lemma2verbif.fst | dot -Tpdf  > lemma2verbif.pdf
 
 
 ################### Testa os tradutores ################
@@ -64,3 +66,9 @@ echo "Output: "
 fstcompose lavar.fst lemma2verbis.fst  > resultado.fst
 fstproject --project_output resultado.fst | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=syms.txt | awk '{print $3}'
 
+
+echo "Transdutor lavar lemma2verbif :"
+fstrmepsilon lavar.fst | fsttopsort | fstprint --isymbols=syms.txt
+echo "Output: "
+fstcompose lavar.fst lemma2verbif.fst  > resultado.fst
+fstproject --project_output resultado.fst | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=syms.txt | awk '{print $3}'
